4a79285203a8edb9a13677f7e1bd3c22e7f7eb77
BhagatHarsh <93080554+BhagatHarsh@users.noreply.github.com>
2023-07-29 15:27:55 +0530

    Update Event_template.js

--- frontend/src/Components/Event_template.js
+++ frontend/src/Components/Event_template.js
@@ -1,8 +1,8 @@
 import Navbar from './BasicExample.js'
-import React, { useEffect, useState } from 'react';
 import T1 from './T1.js';
 import Template_container from './Template_container.js';
 import T_CardContainer from './T_CardContainer.js';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Events() {
@@ -10,6 +10,7 @@ function Events() {
   const { id } = useParams();
 
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   
   useEffect(() => {
     fetch(`http://localhost:3000/package/${id}`, {
@@ -18,13 +19,20 @@ function Events() {
       .then(response => response.json())
       .then(data => {
         setData(data);
+        setIsLoading(false);
         console.log('data: ', data);
       })      
       .catch(error => {
         console.log('Error fetching data:', error);
+        setIsLoading(false);
       });
   }, [id]);
-
+  
+  if(isLoading) {
+    return <div>Loading...</div>;
+  }
+  
+  
   return (
     <>
       <Navbar />
