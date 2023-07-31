2861b54d7e2bcbb850e989ac4cc7f0e0d90e3adf
jineetshah <jineet.s@ahduni.edu.in>
2023-07-31 17:03:11 +0530

    vfdvdf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Events from './Components/Events.js';
 import LoginSignup from './Components/LoginSignup.js';
 import Register from './Components/Register.js';
 import Event_template from './Components/Event_template.js';
+import Profile from './Components/Profile.js';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -20,6 +21,7 @@ function App() {
         <Route path='/LoginSignup' exact element={<LoginSignup/>} />
         <Route path='/register' exact element={<Register/>} />
         <Route path='/Event_template/:id' exact element={<Event_template/>} />
+        <Route path='/Profile' exact element={<Profile/>} />
       </Routes>
       
 
